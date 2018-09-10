0439f5e8b90ab445f90aeebf89c2443ee1b8bc6e
lomasz <lukasz.tomaszewski89@gmail.com>
2018-09-10 22:30:34 +0200

    Created custom logback appender

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # spring-boot-elk
-Spring Boot + ELK
+Spring Boot + ELK with custom fields sending to Logstash:
+ * `serviceName`
+ * `serviceVersion`
+
+##How to run
+
+* Run ELK:
+```
+cd docker
+docker-compose up --build
+```
+
+* Run microservice:
+```
+cd ..
+./gradlew bootRun
+```
+
+* Send request to endpoint:
+```
+curl localhost:8080/api/log
+```
---
+++ src/main/java/com/lomasz/springbootelk/SpringBootElkApplication.java
@@ -0,0 +1,13 @@
+package com.lomasz.springbootelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElkApplication.class, args);
+	}
+}
